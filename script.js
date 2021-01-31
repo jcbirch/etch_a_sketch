e9ceb73c6308a54cc68e79a7cfa07c69e2a70835
jcbirch <jessebirch@icloud.com>
2021-01-31 12:11:15 -0600

    created css grid layout and created js function to change box background on mouseover

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 function createScreen() {
+    
     for (let i = 1; i <= 16; i++) {
         const row = document.createElement("div");
         row.id = "row" + i;
+        row.classList.add("row");
+        const rowStyle = document.querySelector(".row")
+        row.style.gridTemplateColumns = "repeat(16, auto)";
         const elementOne = document.getElementById("the-screen");
         elementOne.appendChild(row);
         for (let j = 1; j <= 16; j++) {
-            const column = document.createElement("div");
-            column.classList.add("column" + j);
+            const box = document.createElement("div");
+            box.classList.add("box");
             const elementTwo = document.getElementById("row" + i);
-            elementTwo.appendChild(column);
+            elementTwo.appendChild(box);
+            changeBoxColor();
         }
-
+        
     }
+}
+
+
+function changeBoxColor() {
+    document.querySelectorAll('.box').forEach(item => {
+        item.addEventListener('mouseover', event => {
+            item.style.backgroundColor = "green";
+        })
+    })
 
 }
+
 
 createScreen();
